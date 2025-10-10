2107f29ab1eb10819a47008b31716bf057edda42
MEMOUE KONE <memouekone@MacBook-Pro-de-MEMOUE.local>
2025-10-10 10:36:42 +0000

    add header

--- EmunieFront/src/app/app.ts
+++ EmunieFront/src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    ButtonModule,
+    InputTextModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('EmunieFront');
+  protected readonly title = signal('Emunie-Market');
+  protected activeTab = signal('accueil');
+  protected mobileMenuOpen = signal(false);
+
+  setActiveTab(tab: string) {
+    this.activeTab.set(tab);
+    this.mobileMenuOpen.set(false);
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen.update(value => !value);
+  }
 }
