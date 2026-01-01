0f9c00c963c52b4939807c0ab3715ef73ea3af33
Memko <memko021@gmail.com>
2026-01-01 20:41:18 +0000

    fix: update files confs environnement

--- EmunieFront/src/environments/environment.prod.ts
+++ EmunieFront/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
-
 export const environment = {
   production: true,
-  apiUrl: 'https://www.emunie-market.com/api',
-  frontendUrl: 'https://www.emunie-market.com',
+  // apiUrl: 'https://mon-domaine.com/api/produit', // URL de production
+  //production: true,
+  apiUrl: 'https://www.eburnie-market.com/api/',
+  frontendUrl: 'https://www.eburnie-market.com',
+  mediaUrl: 'https://www.eburnie-market.com',
   wsUrl: 'wss://www.emunie-market.com',
-
   // Google OAuth Client ID
   // À obtenir depuis: https://console.cloud.google.com/apis/credentials
   googleClientId: '335632105023-hvpq1kbtmmf0uf1ga5126uf5e3iabvul.apps.googleusercontent.com'
-
 };
--- EmunieFront/src/environments/environment.ts
+++ EmunieFront/src/environments/environment.ts
@@ -1,7 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api/',
+//  apiUrl: 'http://localhost:8000/api/',
 
+  // production: true,
+  apiUrl: 'https://www.eburnie-market.com/api/',
+  frontendUrl: 'https://www.eburnie-market.com',
+  wsUrl: 'wss://www.emunie-market.com',
+  mediaUrl: 'https://www.eburnie-market.com',
   // Google OAuth Client ID
   // À obtenir depuis: https://console.cloud.google.com/apis/credentials
   googleClientId: '335632105023-hvpq1kbtmmf0uf1ga5126uf5e3iabvul.apps.googleusercontent.com'
